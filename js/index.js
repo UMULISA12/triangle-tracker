b28e7b56c3628d379305f87e96bf6aa167c606aa
UMULISA12 <email@example.com>
2018-12-17 14:20:15 +0200

    adding my conditions

--- js/index.js
+++ js/index.js
@@ -2,3 +2,24 @@ function checkMyTriangle() {
     var firstSide = document.getElementById("FSide").value;
     var secondSide = document.getElementById("SSide").value;
     var thirdSide = document.getElementById("TSide").value;
+
+    if ((firstSide === thirdSide) && (firstSide=== secondSide) && (secondSide===thirdSide)){
+        alert(" equilateral triangle");
+      }
+     
+     else if ((firstSide===secondSide) ||(firstSide===thirdSide) || (secondSide===thirdSide)) {
+        alert(" Isoscelene triangle");
+     
+     }
+     
+     else if(((firstSide+secondSide) > thirdSide) && ((firstSide+thirdSide) > secondSide) && ((secondSide+thirdSide) > firstSide) && (firstSide!=secondSide) && (firstSide!=thirdSide) && (secondSide!=thirdSide)) {
+       alert(" scalene triangle");
+     }
+     
+     else if (((firstSide+secondSide) <= thirdSide) ||((firstSide+thirdSide)<=secondSide) ||((secondSide+thirdSide) <= firstSide) ) {
+       alert("can not make a triangle");
+     }
+     else {
+       alert("Invalid inputs");
+     }
+     }
