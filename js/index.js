f711d381f330b63fb1245225548bdd3d3c450254
UMULISA12 <email@example.com>
2018-12-17 14:19:10 +0200

    declaring my variables

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
 function checkMyTriangle() {
+    var firstSide = document.getElementById("FSide").value;
+    var secondSide = document.getElementById("SSide").value;
+    var thirdSide = document.getElementById("TSide").value;
