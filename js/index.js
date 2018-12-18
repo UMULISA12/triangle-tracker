4120bab8bf7dec66a9bc7157f44dd189de6fc163
UMULISA12 <email@example.com>
2018-12-18 08:49:19 +0200

    adding gh pages

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
 function checkMyTriangle() {
-    var firstSide = document.getElementById("FSide").value;
-    var secondSide = document.getElementById("SSide").value;
-    var thirdSide = document.getElementById("TSide").value;
+    var firstSide = parseInt(document.getElementById("FSide").value);
+    var secondSide = parseInt(document.getElementById("SSide").value);
+    var thirdSide = parseInt(document.getElementById("TSide").value);
 
     var sidesArray = [firstSide,secondSide,thirdSide];
     sidesArray;
+    
 
-    if (sidesArray[0]+sidesArray[1] < sidesArray[2] && sidesArray[0]+sidesArray[2] < sidesArray[1] && sidesArray[1]+sidesArray[2] < sidesArray[0]){
-        alert("Not a triangle");
+    if (sidesArray[0]+sidesArray[1]>sidesArray[2] && sidesArray[0]+sidesArray[2]>sidesArray[1]&& sidesArray[1]+sidesArray[2]>sidesArray[0] && sidesArray[1]!=sidesArray[2] && sidesArray[0]!=sidesArray[1] && sidesArray[0]!=sidesArray[2]){
+        alert("Those inputs make a Scalene triangle!")
+    
     }else if ((sidesArray[0] === sidesArray[1]) && (sidesArray[1]=== sidesArray[2])){
-        alert("equilateral triangle");
+        alert("Those inputs make equilateral triangle");
     }else if ((sidesArray[0]===sidesArray[1]) ||(sidesArray[0]===sidesArray[2]) || (sidesArray[1]===sidesArray[2])) {
-        alert("Isoscele triangle");
-    }else if (sidesArray[0]+sidesArray[1]>sidesArray[2] && sidesArray[0]+sidesArray[2]>sidesArray[1]&& sidesArray[1]+sidesArray[2]>firstSide && sidesArray[1]!=sidesArray[2] && sidesArray[0]!=sidesArray[1] && sidesArray[0]!=sidesArray[2]){
-         alert("Scalene");
+        alert("Those inputs make Isoscele triangle");
+    }else if (sidesArray[0]+sidesArray[1] < sidesArray[2] || sidesArray[0]+sidesArray[2] < sidesArray[1] || sidesArray[1]+sidesArray[2] < sidesArray[0]){
+        alert("Not a triangle");
      }
     else {
        alert("Invalid inputs to make a triangle");
